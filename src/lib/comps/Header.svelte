1edcfb2771ebf31d4526a433e4adef9ed8964a43
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-30 15:47:04 +0200

    pokakal (#1)
    * pokakal
    
    * Зробив правки

---
+++ src/lib/comps/Header.svelte
@@ -0,0 +1,11 @@
+<header class="container mt-3 flex items-center justify-between">
+	<h1 class="text-3xl font-bold">Logo</h1>
+
+	<div class="flex items-center gap-5">
+		<a href="url">Категорії</a>
+		<a href="url">Кошик</a>
+		<a href="url" class="ml-2">
+			<img src="https://placehold.co/100" alt="" class="size-10 rounded-full" />
+		</a>
+	</div>
+</header>
