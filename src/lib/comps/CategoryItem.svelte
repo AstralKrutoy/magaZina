fdb479cc5ce544b80303885812a8cd05ebd4c9bb
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-30 18:27:04 +0200

    Зробив Категорії

---
+++ src/lib/comps/CategoryItem.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { name, image } = $props();
+</script>
+
+<div class="bg-slate-300">
+	<img src="https://placehold.co/600x400/EEE/31343C" alt="" />
+	<div class="flex items-center justify-center px-4 py-2">
+		<p class="ml-3 font-bold">{name}</p>
+	</div>
+</div>
--- src/lib/comps/Header.svelte
+++ src/lib/comps/Header.svelte
@@ -1,8 +1,8 @@
 <header class="container mt-3 flex items-center justify-between">
-	<h1 class="text-3xl font-bold">Logo</h1>
+	<a class="text-3xl font-bold" href="/">Logo</a>
 
 	<div class="flex items-center gap-5">
-		<a href="url">Категорії</a>
+		<a href="/categories">Категорії</a>
 		<a href="url">Кошик</a>
 		<a href="url" class="ml-2">
 			<img src="https://placehold.co/100" alt="" class="size-10 rounded-full" />
