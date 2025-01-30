3e19b848efd298d24e6468a490ec3d0cc1a6e59a
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-30 14:54:11 +0200

    pokakal

---
+++ src/lib/comps/ProductItem.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Heart } from 'lucide-svelte';
+
+	let { name, price, image } = $props();
+</script>
+
+<!-- <p>{name} - {price}</p> -->
+
+<div class="bg-slate-300">
+	<img src={image} alt="" />
+	<div class="flex items-center justify-between px-4 py-2">
+		<p class="font-bold">{name}</p>
+		<p>{price}$</p>
+	</div>
+	<div class="flex justify-between rounded border px-4 px-6 py-2 py-6 text-black shadow">
+		<button class="rounded bg-white px-3 py-1">Додати в кошик</button>
+		<button>
+			<Heart />
+		</button>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Header from '$lib/comps/Header.svelte';
+	import ProductItem from '../lib/comps/ProductItem.svelte';
+	let tovar = [
+		{
+			id: 1,
+			name: 'unitaz',
+			price: 263,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		},
+		{
+			id: 2,
+			name: 'mashina',
+			price: 789,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		},
+		{
+			id: 3,
+			name: 'dom',
+			price: 2125663,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		},
+		{
+			id: 4,
+			name: 'telephon',
+			price: 896,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		},
+		{
+			id: 5,
+			name: 'knizhka',
+			price: 89,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		},
+		{
+			id: 6,
+			name: 'router',
+			price: 233,
+			img: 'https://placehold.co/600x400/EEE/31343C'
+		}
+	];
+</script>
+
+<Header />
+
+<main class="container">
+	<h2>Популярні товари</h2>
+	<div class="grid grid-cols-4 gap-6">
+		{#each tovar as item}
+			<ProductItem name={item.name} price={item.price} image={item.img} />
+		{/each}
+	</div>
+</main>
