b3610b1bde0a58729563caeca1b9204bbd84a22b
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-30 15:46:25 +0200

    Зробив правки

--- src/lib/comps/ProductItem.svelte
+++ src/lib/comps/ProductItem.svelte
@@ -9,12 +9,12 @@
 <div class="bg-slate-300">
 	<img src={image} alt="" />
 	<div class="flex items-center justify-between px-4 py-2">
-		<p class="font-bold">{name}</p>
+		<p class="ml-3 font-bold">{name}</p>
 		<p>{price}$</p>
 	</div>
-	<div class="flex justify-between rounded border px-4 px-6 py-2 py-6 text-black shadow">
-		<button class="rounded bg-white px-3 py-1">Додати в кошик</button>
-		<button>
+	<div class="flex justify-between gap-4 rounded border px-4 px-6 py-2 py-6 text-black shadow">
+		<button class="w-full rounded bg-white px-7 py-1">Додати в кошик</button>
+		<button class="rounded bg-white px-3 px-4 py-1">
 			<Heart />
 		</button>
 	</div>
