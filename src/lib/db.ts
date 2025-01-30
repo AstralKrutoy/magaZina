56092d030ae1f4aba41d9fdc8d3ffa92788c1414
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-30 20:41:29 +0200

    Рефакторінг

---
+++ src/lib/db.ts
@@ -0,0 +1,91 @@
+export const tovar = [
+    {
+        id: 1,
+        name: 'unitaz',
+        price: 263,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 2,
+        name: 'mashina',
+        price: 789,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 3,
+        name: 'dom',
+        price: 2125663,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 4,
+        name: 'telephon',
+        price: 896,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 5,
+        name: 'knizhka',
+        price: 89,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 6,
+        name: 'router',
+        price: 233,
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    }
+];
+
+export const category = [
+    {
+        id: 1,
+        name: 'Electronics',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 2,
+        name: 'Fashion',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 3,
+        name: 'Home Appliances',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 4,
+        name: 'Books',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 5,
+        name: 'Toys',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 6,
+        name: 'Sports',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 7,
+        name: 'Beauty',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 8,
+        name: 'Health',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 9,
+        name: 'Grocery',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    },
+    {
+        id: 10,
+        name: 'Automotive',
+        img: 'https://placehold.co/600x400/EEE/31343C'
+    }
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,7 @@
 <script>
 	import Header from '$lib/comps/Header.svelte';
 	import ProductItem from '../lib/comps/ProductItem.svelte';
-	let tovar = [
-		{
-			id: 1,
-			name: 'unitaz',
-			price: 263,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		},
-		{
-			id: 2,
-			name: 'mashina',
-			price: 789,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		},
-		{
-			id: 3,
-			name: 'dom',
-			price: 2125663,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		},
-		{
-			id: 4,
-			name: 'telephon',
-			price: 896,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		},
-		{
-			id: 5,
-			name: 'knizhka',
-			price: 89,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		},
-		{
-			id: 6,
-			name: 'router',
-			price: 233,
-			img: 'https://placehold.co/600x400/EEE/31343C'
-		}
-	];
+	import { tovar } from '../lib/db.js';
 </script>
 
 <Header />
