4feaa408d88c4702e975a1ec01faa6292f80d6b9
AstralKrutoy <mrnazarkid@gmail.com>
2025-01-31 12:51:24 +0200

    Mrnazarkid/naz 14 zverstati koshik (#3)
    * Рефакторінг
    
    * Зробив кошик
    
    * піфіксіл

--- src/lib/comps/Header.svelte
+++ src/lib/comps/Header.svelte
@@ -3,7 +3,7 @@
 
 	<div class="flex items-center gap-5">
 		<a href="/categories">Категорії</a>
-		<a href="url">Кошик</a>
+		<a href="/buy">Кошик</a>
 		<a href="url" class="ml-2">
 			<img src="https://placehold.co/100" alt="" class="size-10 rounded-full" />
 		</a>
